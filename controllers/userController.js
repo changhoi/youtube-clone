1dae0a2c77d8fc1e4302798ac142ec3514ab4849
changhoi0522 <changhoi0522@naver.com>
2019-04-15 18:31:46 +0900

    #3.9부터

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,39 +1,33 @@
-import routes from '../routes';
+import routes from "../routes";
 
 export const getJoin = (req, res) => {
-  console.log(req.body);
-  res.render('join', {
-    pageTitle: 'GetJoin'
+  res.render("join", {
+    pageTitle: "GetJoin"
   });
 };
 
 export const postJoin = (req, res) => {
   const {
-    body: {
-      name,
-      email,
-      password,
-      password2
-    }
+    body: { name, email, password, password2 }
   } = req;
   if (password !== password2) {
     res.status(400);
-    res.render('join', {
-      pageTitle: 'join'
+    res.render("join", {
+      pageTitle: "join"
     });
   } else {
     // TODO : Register User
     // TODO : Log user In
     res.redirect(routes.home);
   }
-  res.render('join', {
-    pageTitle: 'PostJoin'
+  res.render("join", {
+    pageTitle: "PostJoin"
   });
 };
 
 export const getLogin = (req, res) =>
-  res.render('login', {
-    pageTitle: 'Login'
+  res.render("login", {
+    pageTitle: "Login"
   });
 export const postLogin = (req, res) => {
   res.redirect(routes.home);
@@ -44,18 +38,19 @@ export const logout = (req, res) => {
   res.redirect(routes.home);
 };
 
-export const users = (req, res) => res.render('users', {
-  pageTitle: 'Users'
-});
+export const users = (req, res) =>
+  res.render("users", {
+    pageTitle: "Users"
+  });
 export const userDetail = (req, res) =>
-  res.render('userDetail', {
-    pageTitle: 'User Detail'
+  res.render("userDetail", {
+    pageTitle: "User Detail"
   });
 export const editProfile = (req, res) =>
-  res.render('editProfile', {
-    pageTitle: 'Edit Profile'
+  res.render("editProfile", {
+    pageTitle: "Edit Profile"
   });
 export const changePassword = (req, res) =>
-  res.render('changePassword', {
-    pageTitle: 'Change Password'
+  res.render("changePassword", {
+    pageTitle: "Change Password"
   });
--- controllers/videoController.js
+++ controllers/videoController.js
@@ -4,7 +4,6 @@ import Video from "../models/Video";
 export const home = async (req, res) => {
   try {
     const videos = await Video.find({});
-    console.log(videos);
 
     return res.render("home", {
       pageTitle: "Home",
@@ -48,10 +47,22 @@ export const postUpload = async (req, res) => {
   res.redirect(routes.videoDetail(newVideo.id));
 };
 
-export const videoDetail = (req, res) =>
-  res.render("videoDetail", {
-    pageTitle: "Video Detail"
-  });
+export const videoDetail = async (req, res) => {
+  const {
+    params: { id }
+  } = req;
+  try {
+    const video = await Video.findById(id);
+    res.render("videoDetail", {
+      pageTitle: "Video Detail",
+      video
+    });
+  } catch (e) {
+    console.log(e);
+    res.redirect(routes.home);
+  }
+};
+
 export const editVideo = (req, res) =>
   res.render("editVideo", {
     pageTitle: "Edit Video"
